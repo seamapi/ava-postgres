f6198a4547076574821e6d744da00a20d7ed96b0
Max Isom <hi@maxisom.me>
2022-09-06 15:40:51 -0500

    Add test

--- src/tests/hooks.test.ts
+++ src/tests/hooks.test.ts
@@ -25,3 +25,41 @@ test("beforeTemplateIsBaked", async (t) => {
   t.true(wasHookCalled)
   await t.notThrowsAsync(async () => await pool.query('SELECT * FROM "foo"'))
 })
+
+test("beforeTemplateIsBaked (params are de-duped)", async (t) => {
+  interface TestFactoryParams {
+    tableName: string
+  }
+
+  const getTestServer = getTestPostgresDatabaseFactory<TestFactoryParams>({
+    key: "beforeTemplateIsBakedDedupeParams",
+    hooks: {
+      beforeTemplateIsBaked: async ({ pool }, { tableName }) => {
+        await pool.query(
+          `CREATE TABLE "${tableName}" ("id" SERIAL PRIMARY KEY)`
+        )
+      },
+    },
+  })
+
+  // Create first template
+  await Promise.all([
+    getTestServer({ tableName: "foo" }),
+    // Re-use created template
+    getTestServer({ tableName: "foo" }),
+    // Create second template
+    getTestServer({ tableName: "bar" }),
+  ])
+
+  const { pool } = await getTestServer({ tableName: "foo" })
+
+  const {
+    rows: [{ count }],
+  } = await pool.query(
+    'SELECT COUNT(*) FROM "pg_database" WHERE "datistemplate" = true'
+  )
+
+  const NUM_OF_DEFAULT_POSTGRES_TEMPLATES = 2
+
+  t.is(parseInt(count, 10), NUM_OF_DEFAULT_POSTGRES_TEMPLATES + 2)
+})
