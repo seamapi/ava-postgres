c4705836b362be396667e1bf618cdda248d67c23
Max Isom <hi@maxisom.me>
2023-06-06 21:24:16 -0700

    Ask clients to disconnect more politely

---
+++ src/tests/cleanup/create-database.ts
@@ -0,0 +1,11 @@
+import test from "ava"
+import { getTestPostgresDatabaseFactory } from "~/index"
+
+const getTestServer = getTestPostgresDatabaseFactory({
+  postgresVersion: process.env.POSTGRES_VERSION,
+})
+
+test("create database", async (t) => {
+  await getTestServer()
+  t.pass()
+})
